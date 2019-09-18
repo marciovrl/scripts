d1f7a3c6e290204bef291389773653bb56758313
marcio lemos <contato.marciovrl@gmail.com>
2019-09-18 14:42:20 -0300

    fix formatting

---  data-validation-with-node/script.js
+++  data-validation-with-node/script.js
@@ -1,46 +1,55 @@
-'use strict'
+"use strict";
 
-console.log("- Iniciando ...")
+console.log("- Iniciando ...");
 
-const xlsx = require('xlsx');
+const xlsx = require("xlsx");
 
-let request = require('request');
+let request = require("request");
 
-console.log("- Criando resultado.txt ....")
-let fs = require('fs');
+console.log("- Criando resultado.txt ....");
+let fs = require("fs");
 let txt = fs.createWriteStream("resultado.txt");
 
-console.log("- Lendo dados.xlxs .....")
-const workbook = xlsx.readFile('dados.xlsx');
+console.log("- Lendo dados.xlxs .....");
+const workbook = xlsx.readFile("dados.xlsx");
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-let url = 'http://localhost:8080/example';
+let url = "http://localhost:8080/example";
 
-function chamando_api(dado, z) { 
-    return request(url+dado, function (error, response, body) {
-        try {
-            let result = JSON.parse(body);
-            txt.write("Linha planilha: " + z + " - Dado: " + dado + " Código Retorno: " + result.cod_retorno +
-            " = Info 01: "+ result.ou_campo_um + " Info 02: " + result.u_campo_um + "\n");
-        }
-        catch (e) {
-            chamando_api(dado, z)
-        }
-    });
-}
-
-function ler_planilha(){
-    for (let z in worksheet) {
-        if(z.toString()[0] === 'A'){
-            chamando_api(worksheet[z].v, z);
-        }
+function chamando_api(dado, z) {
+  return request(url + dado, function(error, response, body) {
+    try {
+      let result = JSON.parse(body);
+      txt.write(
+        "Linha planilha: " +
+          z +
+          " - Dado: " +
+          dado +
+          " Código Retorno: " +
+          result.cod_retorno +
+          " = Info 01: " +
+          result.ou_campo_um +
+          " Info 02: " +
+          result.u_campo_um +
+          "\n"
+      );
+    } catch (e) {
+      chamando_api(dado, z);
     }
+  });
 }
 
-console.log("- Executando Consulta dos dados ......")
-try {
-    ler_planilha();
+function ler_planilha() {
+  for (let z in worksheet) {
+    if (z.toString()[0] === "A") {
+      chamando_api(worksheet[z].v, z);
+    }
+  }
 }
-catch (e) {
-    console.log("Erro ao executar: " + e)
+
+console.log("- Executando Consulta dos dados ......");
+try {
+  ler_planilha();
+} catch (e) {
+  console.log("Erro ao executar: " + e);
 }
